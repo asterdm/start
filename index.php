ee39a84b65e4b95f59b68e3c0a4af8d77d882474
asterdm <asterdm@yandex.ru>
2017-02-21 00:06:55 +0300

    autoload add

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once '/autoload.php';
 
 $ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
 $act = isset($_GET['act']) ? $_GET['act'] : 'all';
@@ -8,7 +8,7 @@ $act = isset($_GET['act']) ? $_GET['act'] : 'all';
 $ctrl .= 'Controller';
 $metod = 'action_'.$act;
 
-require_once __DIR__.'/controllers/'.$ctrl.'.php';
+//require_once __DIR__.'/controllers/'.$ctrl.'.php';
 
 $news = new $ctrl;
 $news->$metod();
